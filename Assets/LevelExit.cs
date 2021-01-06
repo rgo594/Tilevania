fd33cb7c2aae10b520ca0a0fdae91eec9bebdd9c
Rob O'Gara <rgo594@gmail.com>
2021-01-05 23:32:00 -0500

    Added exits

---
+++ Assets/LevelExit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LoadLevelDelay = 2f;
+    [SerializeField] float LevelExitSlowMoFactor = 0.2f;
+    Scene scene;
+
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        Debug.Log(scene.buildIndex);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        Time.timeScale = LevelExitSlowMoFactor;
+        yield return new WaitForSeconds(LoadLevelDelay);
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+}
